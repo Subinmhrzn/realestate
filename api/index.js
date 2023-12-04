5643b7e09c5f5128171d8cd884d8b8c713848e27
Subinmhrzn <subinmhrzn079@gmail.com>
2023-12-04 23:24:23 +0545

    add pre instalation

--- api/index.js
+++ api/index.js
@@ -1,38 +1,50 @@
-import express, { json } from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
-import cookieParser from 'cookie-parser';
-import listingRouter from './routes/listing.route.js'
+import express, { json } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
+import path from "path";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then (()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to MONGODB");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-const app = express ();
+const __dirname = path.resolve();
+
+const app = express();
+
 app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(5000, ()=>{
-    console.log("Server is running in port 5000....")
-})
+app.listen(5000, () => {
+  console.log("Server is running in port 5000....");
+});
 
-app.use('/api/user',userRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/listing', listingRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
-app.use((err, req, res, next)=>{
-    const statusCode =err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        message,
-        statusCode,
-    })
-})
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dest','index.html'))
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
